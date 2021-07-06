d926c29fe53082350f97e69d1c0fe33cf0695ddb
Shefali-Sinha <86995283+Shefali-Sinha@users.noreply.github.com>
2021-07-06 08:39:11 +0530

    Bloomingdale's
    Integration with Footer.

---
+++ Template2/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    if (localStorage.getItem("userChoice") != null) {
+        changeTheme(localStorage.getItem("userChoice"));
+    }
+});
+
+function applyTheme(event) {
+    var color = $(event.target).val();
+    localStorage.setItem("userChoice", color);
+    changeTheme(color);
+}
+
+function changeTheme(color) {
+    $("body").css("background-color", color);
+}
+
+function applyFontSize(event) {
+    var fontSize = $(event.target).val();
+    localStorage.setItem("userFontChoice", fontSize);
+    changeFontSize(fontSize);
+}
+
+function changeFontSize(fontSize) {
+    $(".fontsize-content").css("font-size", fontSize);
+}
+
+function applyFontFamily(event) {
+    var fontFamily = $(event.target).val();
+    changeFontFamily(fontFamily);
+}
+
+function changeFontFamily(fontFamily) {
+    $(".fontsize-content").css("font-family", fontFamily);
+}
